fc6397f752716391bd2b334b9c4332062d8971c4
Nathan El <nathan.ellermeier@duke.edu>
2021-07-17 13:55:15 -0400

    Create function fliptable and add documentation

--- R/tableflip.R
+++ R/tableflip.R
@@ -1,3 +1,35 @@
+#' Flip Table
+#'
+#' @param tab table object
+#'
+#' @return table object
+#' @export
+#'
+#' @examples
+#' set.seed(234)
+#' require(magrittr)
+#'
+#'sample <- dplyr::tibble("A" = stats::rnorm(100, 50, 4.5),
+#'                        "B" = seq(1,300,3))
+#'
+#'sample <- sample %>%
+#'  dplyr::mutate(position = factor(ifelse(B %% 2 == 0, "Even", "Odd")))
+#'
+#'fruits <- c("Kiwi", "Melon", "Peach")
+#'
+#'treatment <- c("Chemical", "Organic", "Squish 'em")
+#'
+#'died <- c("Dead", "Not dead")
+#'
+#'sample$fruit <- sample(fruits, 100, replace = TRUE)
+#'sample$pesticide <- sample(treatment, 100, replace = TRUE)
+#'sample$death <- sample(died, 100, replace = TRUE)
+
+#'tab <- table(sample$pesticide, sample$death, sample$fruit)
+#'
+#'tab
+#'
+#' fliptable(tab)
 fliptable <- function(tab){
   # also need to check if it's a table object? how to do that?
 
